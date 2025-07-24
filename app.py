14f3ded855b48f7cb849093e951c146e6e331b88
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2025-07-23 21:22:42 -0500

    actualizar funcion en app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.conn_Gsheet import conexion_gsheet, abrir_hoja, cargar_datos
+from utils.conn_Gsheet import conexion_gsheet_produccion, abrir_hoja, cargar_datos
 from utils.add_informacion import ingresar_gasto
 from utils.func_dash import aplicar_filtros, mostrar_metricas_clave, graficar_distribucion_categoria, graficar_evolucion_temporal
 from utils.func_dash import graficar_comparativa_persona, graficar_detalle_subcategoria, mostrar_tabla_detallada
